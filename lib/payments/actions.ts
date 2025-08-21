1d6ecc13a063b0dd92cb0bf057f217186d36441f
Juan Manuel Silva <juanmanuelsilva06@gmail.com>
2025-08-21 19:06:41 -0300

    first commit

---
+++ lib/payments/actions.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import {
+  createCheckoutSession,
+  createCustomerPortalSession,
+} from './mercadopago';
+import { withTeam } from '@/lib/auth/middleware';
+
+export const checkoutAction = withTeam(async (formData, team) => {
+  const priceId = formData.get('priceId') as string;
+  await createCheckoutSession({ team: team, priceId });
+});
+
+export const customerPortalAction = withTeam(async (_, team) => {
+  const portalSession = await createCustomerPortalSession(team);
+  redirect(portalSession.url);
+});
